48b33a69b22639bdc6046a859390ec24dc21a00b
Philipp Beck <dertieran@gmail.com>
2021-01-28 16:48:00 +0100

    Decode and encode cookie value

--- www/cookieMaster.js
+++ www/cookieMaster.js
@@ -1,7 +1,23 @@
+
+var decodeCookieValue = function (value) {
+  return value.replace(/(%[\dA-F]{2})+/gi, decodeURIComponent)
+};
+
+var encodeCookieValue = function (value) {
+  return encodeURIComponent(value).replace(
+    /%(2[346BF]|3[AC-F]|40|5[BDE]|60|7[BCD])/g,
+    decodeURIComponent
+  )
+};
+
 var cookieMaster = {
 
     getCookieValue: function(url, cookieName, successCallback, errorCallback) {
-        cordova.exec(successCallback,
+        var success = function (data) {
+          data.cookieValue = decodeCookieValue(data.cookieValue)
+          return successCallback(data);
+        }
+        cordova.exec(success,
                     errorCallback,
                     'CookieMaster', 'getCookieValue',
                     [url, cookieName]
@@ -17,7 +33,7 @@ var cookieMaster = {
         cordova.exec(successCallback,
                     errorCallback,
                     'CookieMaster', 'setCookieValue',
-                    [url, cookieName, cookieValue, cookieOptions || {}]
+                    [url, cookieName, encodeCookieValue(cookieValue), cookieOptions || {}]
         );
     },
     clearCookies: function(successCallback, errorCallback) {
